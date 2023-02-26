38e9b34b149cc2f34751039bc6947e597e226db9
Pedro Ramos <pedroramos.contato@outlook.com>
2023-02-25 23:18:39 -0300

    feat: Add knexfile

---
+++ knexfile.ts
@@ -0,0 +1,22 @@
+import type { Knex } from "knex";
+import path from "path";
+
+const config: { [key: string]: Knex.Config } = {
+	development: {
+		client: "sqlite3",
+		connection: {
+			filename: "./budget.db",
+		},
+		migrations: {
+			directory: path.resolve(
+				__dirname,
+				"src",
+				"infrastructure",
+				"database",
+				"migrations"
+			),
+		},
+	},
+};
+
+module.exports = config;
