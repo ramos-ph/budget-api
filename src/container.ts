de3dc24d1a4bac32fe8b9a2867b48efd638bb518
Pedro Ramos <pedroramos.contato@outlook.com>
2023-02-25 23:49:09 -0300

    feat: Add env separation for container

--- src/container.ts
+++ src/container.ts
@@ -1,6 +1,10 @@
-import { makeCategoryRepository } from "./infrastructure/database/InMemoryCategoryRepository";
+import * as SQLiteCategoryRepository from "./infrastructure/database/category/SQLiteCategoryRepository";
+import * as InMemoryCategoryRepository from "./infrastructure/database/category/InMemoryCategoryRepository";
 
-const categoryRepository = makeCategoryRepository();
+const categoryRepository =
+	process.env.NODE_ENV === "test"
+		? InMemoryCategoryRepository.makeCategoryRepository()
+		: SQLiteCategoryRepository.makeCategoryRepository();
 
 const container = {
 	categoryRepository,
