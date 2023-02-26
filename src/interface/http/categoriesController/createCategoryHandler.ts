d3b693ce3ed906a56a20300e03d75cebe5b35f0b
Pedro Ramos <pedroramos.contato@outlook.com>
2023-02-26 01:13:19 -0300

    feat: Add zod validation to create category handler

--- src/interface/http/categoriesController/createCategoryHandler.ts
+++ src/interface/http/categoriesController/createCategoryHandler.ts
@@ -1,20 +1,31 @@
 import { RequestHandler } from "express";
+import { z } from "zod";
 
 import { makeCreateCategory } from "../../../application/categories/use-cases/CreateCategory";
 
-export const createCategoryHandler: RequestHandler = async (req, res) => {
-	const { name, description, monthly_limit } = req.body;
+const schema = z
+	.object({
+		name: z
+			.string({ required_error: "Cannot be empty" })
+			.min(1, "Cannot be empty"),
+		description: z.string().optional(),
+		monthly_limit: z
+			.number({
+				required_error: "Cannot be empty",
+			})
+			.positive("Must be a positive integer")
+			.int("Must be a positive integer"),
+	})
+	.required({ name: true, monthly_limit: true });
 
+export const createCategoryHandler: RequestHandler = async (req, res, next) => {
 	const createCategory = makeCreateCategory(req.container);
 
 	try {
-		const categories = await createCategory({
-			name,
-			description,
-			monthly_limit,
-		});
+		const category = schema.parse(req.body);
+		const categories = await createCategory(category);
 		return res.status(201).send(categories);
 	} catch (err) {
-		return res.status(422).send({ message: err.message });
+		return next(err);
 	}
 };
